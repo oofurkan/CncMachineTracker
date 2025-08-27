7338a2f6d798c3d427d171b304e2715a58e39290
DESKTOP-IGMIFTE\Ömer Furkan <omerfurkankeltek@gmail.com>
2025-08-27 19:49:37 +0300

    machine simulate update

--- CncMachineTracker.Api/Services/MachineService.cs
+++ CncMachineTracker.Api/Services/MachineService.cs
@@ -26,17 +26,47 @@ namespace CncMachineTracker.Api.Services
 			var rnd = new Random();
 			var statuses = new[] { "Çalışıyor", "Duruşta", "Alarm" };
 
-			var machine = new Machine
+			// Generate a machine ID - sometimes use existing ones, sometimes create new ones
+			string machineId;
+			if (_machines.Count > 0 && rnd.Next(0, 100) < 60) // 60% chance to update existing machine
 			{
-				Id = $"M{rnd.Next(100, 999)}",
-				Status = statuses[rnd.Next(statuses.Length)],
-				ProductionCount = rnd.Next(0, 1000),
-				CycleTime = Math.Round(rnd.NextDouble() * 60, 1),
-				Timestamp = DateTime.Now
-			};
+				// Pick a random existing machine to update
+				machineId = _machines[rnd.Next(_machines.Count)].Id;
+			}
+			else
+			{
+				// Create a new machine ID
+				machineId = $"M{rnd.Next(100, 999)}";
+			}
 
-			_machines.Add(machine);
-			return machine;
+			// Check if machine with this ID already exists
+			var existingMachine = _machines.FirstOrDefault(m => m.Id.Equals(machineId, StringComparison.OrdinalIgnoreCase));
+
+			if (existingMachine != null)
+			{
+				// Update existing machine with new random data
+				existingMachine.Status = statuses[rnd.Next(statuses.Length)];
+				existingMachine.ProductionCount = rnd.Next(0, 1000);
+				existingMachine.CycleTime = Math.Round(rnd.NextDouble() * 60, 1);
+				existingMachine.Timestamp = DateTime.Now;
+				
+				return existingMachine;
+			}
+			else
+			{
+				// Create new machine
+				var newMachine = new Machine
+				{
+					Id = machineId,
+					Status = statuses[rnd.Next(statuses.Length)],
+					ProductionCount = rnd.Next(0, 1000),
+					CycleTime = Math.Round(rnd.NextDouble() * 60, 1),
+					Timestamp = DateTime.Now
+				};
+
+				_machines.Add(newMachine);
+				return newMachine;
+			}
 		}
 	}
 }
